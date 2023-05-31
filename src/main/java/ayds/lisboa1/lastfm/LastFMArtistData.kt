cadc20df875464cf54e09c72274b820660074df6
Federico Arias <ariasfede596@gmail.com>
2023-05-31 10:13:56 -0300

    Agregado de LastFM  image

--- src/main/java/ayds/lisboa1/lastfm/LastFMArtistData.kt
+++ src/main/java/ayds/lisboa1/lastfm/LastFMArtistData.kt
@@ -1,5 +1,6 @@
 package ayds.lisboa1.lastfm
 
+const val LASTFM_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Lastfm_logo.svg/320px-Lastfm_logo.svg.png"
 data class LastFMArtistData (
     var artistName: String,
     var artisInfo: String,
